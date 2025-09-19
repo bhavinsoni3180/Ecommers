7c7e4eb3eae25f6840677227a80c494ecefd4e7b
bhavinsoni3180 <69751313+bhavinsoni3180@users.noreply.github.com>
2025-09-19 20:35:06 +1200

    Add lucide-react, react-icons and location in to navbar page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,22 @@
+import { MapPin } from 'lucide-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function navbar() {
   return (
     <div className='bg-white py-3 shadow-2xl'>
-      this is navbar
+        <div className='max-w-7xl mx-auto flex justify-between items-center'>
+            {/* Logo Section */}
+            <div className=''>
+                <Link to={'/'}><h1 className='font-bold text-3xl'><span className='text-red-500 font-serif'>BS</span> Shopping</h1></Link>
+                <div className='flex gap-1 cursor-pointer text-gray-700 items-center'>
+                    <MapPin className='text-red-500'/>
+                    <span className='font-semibold'>
+                        {location ? <div> </div>:"Add Address"}
+                    </span>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
